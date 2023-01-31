da8f7ece45f2cd2f354e074546a515ec548002a9
KanishkVijaywargiya <secretcodersociety@gmail.com>
2023-01-31 14:20:38 +0530

    Initial Commit

---
+++ ImagesCoreData/ImagesCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesCoreDataApp.swift
+//  ImagesCoreData
+//
+//  Created by Kanishk Vijaywargiya on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ImagesCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
